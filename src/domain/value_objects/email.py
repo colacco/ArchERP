50475acab48803939d619c8f1574e7db17624e4d
colaco <gabrielramoscolaco@gmail.com>
2026-06-16 00:37:16 -0300

    feat: create user entity

---
+++ src/domain/value_objects/email.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+import re
+
+@dataclass(frozen=True)
+class Email:
+    value: str
+
+    @staticmethod
+    def _is_valid(email: str) -> bool:
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        return bool(re.match(pattern, email))
+
+    def __post_init__(self):
+        normalized = self.value.strip().lower()
+
+        if not self._is_valid(normalized):
+            raise ValueError("Invalid Email")
+        
+        object.__setattr__(self, "value", normalized)
+
+    def __str__(self) -> str:
+        return self.value
