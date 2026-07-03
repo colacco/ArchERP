73a7fb7c86af6d475820b93002bc8c2e55e7c843
collacco <gabriel.colaco@solutionsbi.com.br>
2026-07-03 13:31:54 -0300

    feat(password): Add hash password for User

--- src/application/use_cases/create_user.py
+++ src/application/use_cases/create_user.py
@@ -4,12 +4,14 @@ from datetime import datetime
 from src.domain.entities.user import User
 from src.domain.value_objects.email import Email
 from src.domain.repositories.user_repository import UserRepository
+from src.application.ports.password_hasher import PasswordHasher
 from src.application.dtos.create_user_input import CreateUserInput
 from src.application.dtos.user_output import UserOutput
 
 class CreateUser():
-    def __init__(self, repository: UserRepository):
+    def __init__(self, repository: UserRepository, password_hasher: PasswordHasher):
         self._repository = repository
+        self._hasher = password_hasher
 
     def execute(self, dto: CreateUserInput):
         
@@ -18,7 +20,7 @@ class CreateUser():
             role_id= uuid4(),
             name= dto.name,
             email= Email(dto.email),
-            password_hash= "",
+            password_hash= self._hasher.hash(dto.password),
             created_at= datetime.now(),
             updated_at= datetime.now()
         )
--- src/presentation/http/dependencies.py
+++ src/presentation/http/dependencies.py
@@ -5,11 +5,16 @@ from src.application.use_cases.update_user import UpdateUser
 from src.application.use_cases.delete_user import DeleteUser
 
 from src.infrastructure.in_memory.user_repository import InMemoryUserRepository
+from src.infrastructure.security.bcrypt_password_hasher import BcryptPasswordHasher
 
 _repository = InMemoryUserRepository()
+_hasher = BcryptPasswordHasher()
 
 def get_user_repository():
     return _repository
+
+def get_password_hasher():
+    return _hasher
 
 def make_get_users_use_case():
     return GetUsers(get_user_repository())
@@ -18,7 +23,7 @@ def make_get_user_use_case():
     return GetUser(get_user_repository())
 
 def make_create_user_use_case():
-    return CreateUser(get_user_repository())
+    return CreateUser(get_user_repository(), get_password_hasher())
 
 def make_update_user_use_case():
     return UpdateUser(get_user_repository())
