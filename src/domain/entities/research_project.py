d90fd267e2031ec202feebdc79344073885d8831
collacco <gabriel.colaco@solutionsbi.com.br>
2026-07-21 13:42:40 -0300

    feat(research project): add entites, exceptions, resository and create enums folder for research project at domain

---
+++ src/domain/entities/research_project.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+from src.domain.enums.research_project_status import ResearchProjectStatus
+from src.domain.enums.research_project_visibility import ResearchProjectVisibility
+
+@dataclass
+class ResearchProject():
+    id: UUID
+    owner_id: UUID
+
+    title: str
+    description: str
+    objective: str
+    research_area: str
+    sub_area: str
+    instituition: str
+    keywords: list[str]
+    status: ResearchProjectStatus
+    visibility: ResearchProjectVisibility
+
+    created_at: datetime
+    updated_at: datetime
+    finished_at: datetime | None
---
+++ src/domain/enums/research_project_visibility.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class ResearchProjectVisibility(str, Enum):
+    TEAM = "team"
+    PUBLIC = "public"
